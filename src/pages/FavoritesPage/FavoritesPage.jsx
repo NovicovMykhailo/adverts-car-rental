25600f7888243de840451c74f54edbd6dfaff709
NovicovMykhailo <mikenovicov@gmail.com>
2023-09-11 21:13:21 +0300

    Update FavoritesPage.jsx

--- src/pages/FavoritesPage/FavoritesPage.jsx
+++ src/pages/FavoritesPage/FavoritesPage.jsx
@@ -1,5 +1,9 @@
 export default function FavoritesPage() {
-    return <div>Favorites Page</div>;
-  }
+  return (
+    <div>
   
-  
+      <h1 className="visually-hidden">Car Rantal Catalog</h1>
+      Favorites Page
+    </div>
+  );
+}
