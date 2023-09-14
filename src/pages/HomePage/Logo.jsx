1613ba6d096527e35296845e3b916b724ce3a68b
NovicovMykhailo <mikenovicov@gmail.com>
2023-09-14 15:07:31 +0300

    Update Logo.jsx

--- src/pages/HomePage/Logo.jsx
+++ src/pages/HomePage/Logo.jsx
@@ -2,7 +2,7 @@ import logo from '../../assets/logo.svg';
 import css from './HomePage.module.css'
 export default function Logo() {
   return (
-    <div style={{display: "flex", flexDirection: "colum",alignItems: "center", justifyContent:"space-between", width: "164px"}} className={css.logo}>
+    <div style={{display: "flex", flexDirection: "colum",alignItems: "center", justifyContent:"space-between", width: "111px"}} className={css.logo}>
       <img src={logo} alt="Logo" width="50" height="50"/>
       <p style={{fontWeight: "600",}}>Phenix Rentals</p>
     </div>
